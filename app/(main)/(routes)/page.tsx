b03f561c35d97b89b4946474f7a8f322b50cbf12
pmjames29 <pmjames29@gmail.com>
2024-02-16 09:40:14 -0500

    Added user authentication and light/dark mode setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,13 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
+
 const state = true;
 
 export default function Home() {
   return (
     <div >
-      This is a protected route.
+      <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
     
   );
